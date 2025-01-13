55e156788d1f0d5d56a9691dfb150c68fd74b44c
aryantandon2323 <aryantandon2323@gmail.com>
2025-01-13 17:31:39 +0000

    modified:   online_research.py

--- online_research.py
+++ online_research.py
@@ -3,6 +3,16 @@ from phi.tools.arxiv_toolkit import ArxivToolkit
 from phi.model.groq import Groq
 from phi.tools.googlesearch import GoogleSearch
 
+import phi
+import phi.api
+import dotenv
+import os
+from dotenv import load_dotenv
+
+#TO load environment variables from .env file
+load_dotenv()
+
+phi.api= os.getenv("OPENAI_API_KEY")
 #Arxiv Agent
 research_agent=Agent(
     name='research Agent for arxiv documents',
@@ -12,7 +22,22 @@ research_agent=Agent(
     markdown=True
 )
 
+web_agent= Agent(
+    name='Web search Agent',
+    tools=[GoogleSearch()],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    instructions=["Search for research papers from google scholar, semantic scholar, ieeexplore and paperswithcode.com for topics and methodogies used."],
+    show_tool_calls=True,
+    markdown=True
+)
 
 
+
+multi_agent= Agent(
+    team= [research_agent,web_agent],
+    instructions= ["Analyse and give sources for each method you mention","Analyse the methodolgy and give sources for it, and the recent trends in the relevatn domain and include some prospective topics as well"]
+)
 #agent = Agent(tools=[ArxivToolkit()], show_tool_calls=True)
-research_agent.print_response("Search arxiv for 'Optical Character Recognition','Big data' and 'LLMs", markdown=True)
+#research_agent.print_response("Search arxiv for 'Optical Character Recognition','Big data' and 'LLMs", markdown=True)
+
+multi_agent.print_response("Search for research topics in OCR and LLM combination",markdown=True)
